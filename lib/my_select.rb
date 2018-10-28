3d00122baa364f66157d5801a282acd0563366a7
John Martinez <jmartinez729@gmail.com>
2018-10-28 17:33:31 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+ i = 0
+ final_collection = []
+ 
+ while i < collection.size
+    if collection[i] == true
+      final_collection << yield(collection[i])
+    end 
+    i += 1
+  end 
+  final_collection
 end
