8feaa6101d707983f741f20db0a0d125b055d48c
John Martinez <jmartinez729@gmail.com>
2018-10-28 17:34:33 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
  
  while i < collection.size
     if yield(collection[i]) == true
-      final_collection << yield(collection[i])
+      final_collection << collection[i]
     end 
     i += 1
   end 
